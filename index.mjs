dc7c48ba15a101fa0bcda95ea8e2750ce2b226de
seandunaway <git@seandunaway.com>
2023-09-03 21:10:48 -0700

    init

---
+++ index.mjs
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import {createServer} from 'node:http'
+
+let whitelist = [
+    'https://query1.finance.yahoo.com/v8/finance/chart/',
+]
+
+let port = process.argv[2] ?? 8888
+let server = createServer()
+server.listen(port)
+
+server.on('listening', function () {
+    console.info(new Date(), 'cors-light', port)
+})
+
+server.on('request', async function (request, response) {
+    if (!request.url) return false
+
+    let url = request.url.slice(1, request.url.length)
+    for (let item of whitelist) {
+        if (!url.startsWith(item)) {
+            console.error('!', url)
+            response.statusCode = 403
+            response.end()
+            return
+        }
+    }
+
+    console.info(url)
+    let fetch_response = await fetch(url)
+    let fetch_text = await fetch_response.text()
+    response.end(await fetch_text)
+})
+
+// @todo proxy method and body
